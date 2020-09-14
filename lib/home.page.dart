8a80d1a6bff14c34f4e3ecb358cefe86851db2c9
Henrique Dezani <dezani@gmail.com>
2020-09-14 20:38:28 -0300

    Aula do dia 14/09

--- lib/home.page.dart
+++ lib/home.page.dart
@@ -7,7 +7,31 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Home Page"),
       ),
-      body: Text("Home Page"),
+      body: Container(
+        margin: EdgeInsets.fromLTRB(20, 20, 20, 10),
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(4, 4),
+              color: Color.fromRGBO(0, 0, 0, 0.1),
+              blurRadius: 6,
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(
+                'https://via.placeholder.com/100x100.png?text=Produto1'),
+            SizedBox(width: 10),
+            Text("Nome do Produto"),
+          ],
+        ),
+      ),
     );
   }
 }
